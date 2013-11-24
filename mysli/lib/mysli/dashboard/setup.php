b21088cd476aaae3b861843a0d532b4a105dfdf5
Marko Gajst <m@avrelia.com>
2013-11-24 23:28:13 +0100

    Asset files will be copied to public folder.

--- mysli/lib/mysli/dashboard/setup.php
+++ mysli/lib/mysli/dashboard/setup.php
@@ -3,4 +3,19 @@
 namespace Mysli\Dashboard;
 
 class Setup
-{}
+{
+    public function before_enable()
+    {
+        \FS::dir_copy(
+            libpath('mysli/dashboard/assets'),
+            pubpath('mysli/dashboard'))
+        );
+        return true;
+    }
+
+    public function after_disable()
+    {
+        \FS::dir_remove(pubpath('mysli/dashboard'));
+        return true;
+    }
+}
