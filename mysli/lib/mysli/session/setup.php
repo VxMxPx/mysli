82e50b179d16704275ca517fdfa5ac5899acf4e7
Marko <m@avrelia.com>
2014-01-10 22:13:31 +0100

    Session initial, non functional.

--- mysli/lib/mysli/session/setup.php
+++ mysli/lib/mysli/session/setup.php
@@ -3,4 +3,15 @@
 namespace Mysli\Session;
 
 class Setup
-{}
+{
+    public function before_enable()
+    {
+        \FS::dir_create(datpath('session'), \FS::EXISTS_MERGE);
+        return true;
+    }
+
+    public function before_disable()
+    {
+        return \FS::dir_remove(datpath('session'));
+    }
+}
