f3d41a95cf7582135adab705f32bd3b24586bf5a
Marko <m@avrelia.com>
2014-02-19 22:03:18 +0100

    Dashboard basic functionallity.

--- mysli/lib/mysli/dashboard/dashboard.php
+++ mysli/lib/mysli/dashboard/dashboard.php
@@ -4,7 +4,21 @@ namespace Mysli;
 
 class Dashboard
 {
-    public function display()
+    protected $web;
+    protected $output;
+
+    public function __construct($web, $router, $event, $output)
     {
+        $this->web = $web;
+        $this->output = $output;
+    }
+
+    public function display($response)
+    {
+        $response->status_200_ok();
+        $this->output->add(
+            '<h1>Hi! I\'m a Dashboard!</h1>',
+            'mysli/dashboard/display'
+        );
     }
 }
--- mysli/lib/mysli/dashboard/setup.php
+++ mysli/lib/mysli/dashboard/setup.php
@@ -4,18 +4,29 @@ namespace Mysli\Dashboard;
 
 class Setup
 {
+    protected $web;
+    protected $event;
+
+    public function __construct($web, $router, $event, $output)
+    {
+        $this->web = $web;
+        $this->event = $event;
+    }
+
     public function before_enable()
     {
         \Core\FS::dir_copy(
             libpath('mysli/dashboard/assets'),
-            pubpath('mysli/dashboard')
+            $this->web->path('mysli/dashboard')
         );
+        $this->event->register('*/router/route:*<dashboard*>', 'mysli/dashboard::display');
         return true;
     }
 
-    public function after_disable()
+    public function before_disable()
     {
-        \Core\FS::dir_remove(pubpath('mysli/dashboard'));
+        \Core\FS::dir_remove($this->web->path('mysli/dashboard'));
+        $this->event->unregister('*/router/route:*<dashboard*>', 'mysli/dashboard::display');
         return true;
     }
 }
