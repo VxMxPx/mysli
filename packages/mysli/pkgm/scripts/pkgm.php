733d2b053d928cebd0458f26f75dce9747aafd96
Marko <m@avrelia.com>
2014-02-23 23:15:04 +0100

    Fixed: after_disable not run if after_enable don't exists.

--- packages/mysli/pkgm/scripts/pkgm.php
+++ packages/mysli/pkgm/scripts/pkgm.php
@@ -22,6 +22,7 @@ class Pkgm
                 'list'    => 'Will list enabled / disabled packages.'
             ]
         );
+
         return true;
     }
 
@@ -32,15 +33,19 @@ class Pkgm
             'packages enable <PACKAGE NAME>'
         );
         \Cli\Util::plain('Example: packages enable mysli/config');
+
         return true;
     }
+
     protected function enable_helper($pkg)
     {
         $setup = $this->pkgm->construct_setup($pkg);
+
         if (method_exists($setup, 'before_enable') && !$setup->before_enable()) {
             \Cli\Util::error('Setup failed for: ' . $pkg);
             return false;
         }
+
         if (!$this->pkgm->enable($pkg)) {
             \Cli\Util::error('Failed to enable: ' . $pkg);
             return false;
@@ -52,40 +57,51 @@ class Pkgm
             return true;
         }
     }
+
     public function action_enable($pkg)
     {
         if (!$pkg) {
             return $this->help_enable();
         }
+
         if ($this->pkgm->is_enabled($pkg)) {
             \Cli\Util::warn("Package is already enabled: `{$pkg}`.");
             return false;
         }
+
         if (!$this->pkgm->resolve($pkg, 'disabled')) {
             \Cli\Util::warn("Package not found: `{$pkg}`.");
             return false;
         }
+
         $dependencies = $this->pkgm->get_dependencies($pkg, true);
+
         if (!empty($dependencies['missing'])) {
-            \Cli\Util::warn('Cannot enable, following packages are missing: ' .
-                print_r($dependencies['missing'], true));
+            \Cli\Util::warn(
+                "Cannot enable, following packages are missing: \n\n" .
+                \Core\Arr::readable($dependencies['missing'], 2) . "\n"
+            );
             return false;
         }
+
         if (count($dependencies['disabled'])) {
             \Cli\Util::plain(
                 "The following packages needs to be enabled: \n\n" .
                 \Core\Arr::readable($dependencies['disabled'], 2) . "\n"
             );
+
             if (!\Cli\Util::confirm('Continue and enable required packages?')) {
                 \Cli\Util::plain('Process terminated.');
                 return false;
             }
+
             foreach ($dependencies['disabled'] as $dependency => $version) {
                 if (!$this->enable_helper($dependency)) {
                     return false;
                 }
             }
         }
+
         return $this->enable_helper($pkg);
     }
 
@@ -96,51 +112,62 @@ class Pkgm
             'packages disable <PACKAGE NAME>'
         );
         \Cli\Util::plain('Example: packages disable mysli/config');
+
         return true;
     }
+
     protected function disable_helper($pkg)
     {
         $setup = $this->pkgm->construct_setup($pkg);
+
         if (method_exists($setup, 'before_disable') && !$setup->before_disable()) {
             \Cli\Util::error('Setup failed for: ' . $pkg);
             return false;
         }
+
         if (!$this->pkgm->disable($pkg)) {
             \Cli\Util::error('Failed to disable: ' . $pkg);
             return false;
         } else {
             \Cli\Util::success('Disabled: ' . $pkg);
-            if (method_exists($setup, 'after_enable')) {
+            if (method_exists($setup, 'after_disable')) {
                 $setup->after_disable();
             }
             return true;
         }
     }
+
     public function action_disable($pkg)
     {
         if (!$pkg) {
             return $this->help_disable();
         }
+
         if (!$this->pkgm->is_enabled($pkg)) {
             \Cli\Util::warn("Package not enabled: `{$pkg}`.");
             return false;
         }
+
         $dependees = $this->pkgm->get_dependees($pkg, true);
+
         if (!empty($dependees)) {
             \Cli\Util::plain(
                 "The following packages depends on the `{$pkg}` and need to be disabled:\n\n" .
                 \Core\Arr::readable($dependees, 2) . "\n"
             );
+
             if (!\Cli\Util::confirm('Disable listed packages?')) {
                 \Cli\Util::plain('Process terminated.');
                 return false;
             }
+
             foreach ($dependees as $dependee) {
                 if (!$this->disable_helper($dependee)) {
                     return false;
                 }
             }
         }
+
         return $this->disable_helper($pkg);
     }
 
@@ -151,27 +178,33 @@ class Pkgm
             'packages list <disabled|enabled>'
         );
         \Cli\Util::plain('Example, list all disabled packages: packages list disabled');
+
         return true;
     }
+
     public function action_list($type = null)
     {
         switch (strtolower($type)) {
             case 'enabled':
                 \Cli\Util::nl();
-                \Cli\Util::plain(\Core\Arr::readable($this->pkgm->get_enabled(), 2), true);
+                \Cli\Util::plain(
+                    \Core\Arr::readable($this->pkgm->get_enabled(), 2),
+                    true
+                );
                 break;
 
             case 'disabled':
                 \Cli\Util::nl();
-                \Cli\Util::plain(\Core\Arr::readable(
-                    $this->pkgm->get_disabled(),
-                    ' : ', "\n", 2, 2
-                ), true);
+                \Cli\Util::plain(
+                    \Core\Arr::readable($this->pkgm->get_disabled(), 2),
+                    true
+                );
                 break;
 
             default:
                 $this->help_list();
         }
+
         return true;
     }
 }
