b337aef68ec5b841f51b8bdc5a06537ed8a413c8
Marko <m@avrelia.com>
2014-03-31 17:46:33 +0200

    Fork command method added.

--- packages/mysli/dot/util.php
+++ packages/mysli/dot/util.php
@@ -254,4 +254,34 @@ class Util
 
         flush();
     }
+
+    /**
+     * This will fork execute command, and stop execution in child.
+     * Please use 'pcntl_wait($status);' in parent, to prevent zombie processes!
+     * --
+     * @param  mixed   $commands string|array
+     * @param  boolean $print    Print result of script execution (use system rather than exec).
+     * --
+     * @return integer pid
+     */
+    public static function fork_command($commands, $print = true)
+    {
+        $pid = pcntl_fork();
+        if ($pid === -1) {
+            die(self::error('Cannot fork the process...'));
+        } else if ($pid === 0) {
+            // We are the child, execute command and quit!
+            if (!is_array($commands)) $commands = [$commands];
+            foreach ($commands as $command) {
+                if ($print) {
+                    system($command);
+                } else {
+                    exec($command);
+                }
+            }
+            exit(1);
+        } else {
+            return $pid;
+        }
+    }
 }
