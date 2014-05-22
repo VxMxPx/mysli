b2a4689ec8189afc82ccf7ae82e286b2783dc49c
Marko <m@gaj.st>
2014-05-22 16:39:05 +0200

    Update: panels.

---
+++ packages/mysli/dashboard/assets/dist/js/mysli.js
@@ -0,0 +1,1 @@
+!function(){var panel=new MU.Panel(".panel.primary");window.MD={panel:panel,updateUsername:function(){}}}(Zepto);
---
+++ packages/mysli/dashboard/assets/src/js/mysli/mysli.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+    var panel = new MU.Panel('.panel.primary');
+
+    window.MD = {
+        panel          : panel,
+        updateUsername : function () {
+
+        }
+    };
+
+}(Zepto));
