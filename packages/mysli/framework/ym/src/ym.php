797760e34e73984c89a3289c37dee3f92910ccc7
Marko <m@gaj.st>
2014-10-30 15:59:46 +0100

    Nicer error reporting.

--- packages/mysli/framework/ym/src/ym.php
+++ packages/mysli/framework/ym/src/ym.php
@@ -15,7 +15,12 @@ class ym {
      * @return array
      */
     static function decode_file($filename) {
-        return self::decode(file::read($filename));
+        try {
+            return self::decode(file::read($filename));
+        } catch (framework\exception\parser $e) {
+            throw new framework\exception\parser(
+                $e->getMessage()."\nFile: {$filename}");
+        }
     }
     /**
      * Decode .ym string.
@@ -27,15 +32,15 @@ class ym {
         if (!trim($string)) {
             return [];
         }
+
         $list = [];
         $stack = [&$list];
         $indent = self::detect_indent($string);
         $level  = 0;
         $string = str::to_unix_line_endings($string);
-        $cl = 0;
-        foreach (explode("\n", $string) as $line) {
-            // Current line +`
-            $cl++;
+        $lines = explode("\n", $string);
+
+        foreach ($lines as $lineno => $line) {
             // An empty line
             if (!trim($line)) { continue; }
             // Comment
@@ -44,29 +49,34 @@ class ym {
             $level = $indent ? self::get_level($line, $indent) : 0;
 
             $stack = array_slice($stack, 0, $level+1);
-            // List item...
-            if (substr(trim($line), 0, 1) === '-') {
-                list($key, $value) = self::proc_line(
-                    ltrim($line, "\t -"), true, $cl);
-                // just one - meaning sub category
-                if (!($key.$value)) {
-                    $key = count($stack[$level]);
+            try {
+                // List item...
+                if (substr(trim($line), 0, 1) === '-') {
+                    list($key, $value) = self::proc_line(
+                        ltrim($line, "\t -"), true);
+                    // just one - meaning sub category
+                    if (!($key.$value)) {
+                        $key = count($stack[$level]);
+                        $stack[$level][$key] = [];
+                        $stack[] = &$stack[$level][$key];
+                    } else {
+                        $key
+                            ? $stack[$level][$key] = $value
+                            : $stack[$level][] = $value;
+                    }
+                    continue;
+                }
+
+                list($key, $value) = self::proc_line($line, false);
+                if ($value === null) {
                     $stack[$level][$key] = [];
                     $stack[] = &$stack[$level][$key];
                 } else {
-                    $key
-                        ? $stack[$level][$key] = $value
-                        : $stack[$level][] = $value;
+                    $stack[$level][$key] = $value;
                 }
-                continue;
-            }
-
-            list($key, $value) = self::proc_line($line, false, $cl);
-            if ($value === null) {
-                $stack[$level][$key] = [];
-                $stack[] = &$stack[$level][$key];
-            } else {
-                $stack[$level][$key] = $value;
+            } catch (\Exception $e) {
+                throw new framework\exception\parser(
+                    $e->getMessage()."\n".self::err_lines($lines, $lineno));
             }
         }
         return $list;
@@ -75,18 +85,16 @@ class ym {
      * Extract key / value from line!
      * @param  string  $line
      * @param  boolean $li   list item?
-     * @param  integer $cl   current line
      * @return array
      */
-    private static function proc_line($line, $li, $cl) {
+    private static function proc_line($line, $li) {
         $segments = explode(':', trim($line), 2);
         $key   = null;
         $value = null;
         if (!isset($segments[1])) {
             if (!$li) {
                 throw new framework\exception\data(
-                    "Error unexpected value: `{$line}` on line: ".
-                    "`{$cl}`. Colon (:) is required.", 1);
+                    "Missing colon (:) or dash (-).", 1);
             } else {
                 $value = $segments[0];
             }
@@ -139,5 +147,31 @@ class ym {
         if (preg_match('/(^[ \t]+)/m', $string, $matches)) {
             return $matches[1];
         }
+    }
+    /**
+     * Return -$padding, $current, +$padding lines for exceptions, e.g.:
+     *   11. ::if true
+     * >>12.     {username|non_existant_function}
+     *   13. ::/if
+     * @param  array   $lines
+     * @param  integer $current
+     * @param  integer $padding
+     * @return string
+     */
+    private static function err_lines($lines, $current, $padding=3) {
+        $start    = $current - $padding;
+        $end      = $current + $padding;
+        $result   = '';
+        for ($position = $start; $position <= $end; $position++) {
+            if (isset($lines[$position])) {
+                if ($position === $current) {
+                    $result .= ">>";
+                } else {
+                    $result .= "  ";
+                }
+                $result .= ($position+1).". {$lines[$position]}\n";
+            }
+        }
+        return $result;
     }
 }
