3b63e7bd6bd2c29d7ac5dfa2115d488a31676170
Marko <m@gaj.st>
2014-11-12 23:30:29 +0100

    A simple & small curl wrapper.

---
+++ packages/mysli/util/curl/src/exception/curl.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace mysli\util\curl\exception {
+    class curl extends \mysli\framework\exception\base {}
+}
---
+++ packages/mysli/util/curl/src/setup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace mysli\util\curl\setup;
+
+__use(__namespace__, '
+    mysli/util/config
+    mysli/framework/fs/fs,dir
+');
+
+function enable() {
+    $c = config::select('mysli/util/curl');
+    $c->merge([
+        // Weather to acuire an agent from user...
+        'user_agent' => true,
+        // If user's agent's not set, or set to false, what to use as a fallback
+        'costume_agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0',
+        // Cokie will be stored in {datpath}/{cookie_filename}
+        'cookie_filename' => 'default.txt',
+        // CURL overwrites, be very careful with those!
+        // Onlt applied when calling ::post, ::get
+        'default' => [
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_AUTOREFERER    => true,
+            CURLOPT_CONNECTTIMEOUT => 8,
+            CURLOPT_TIMEOUT        => 8,
+            CURLOPT_MAXREDIRS      => 8
+        ]
+    ]);
+    return dir::create(fs::datpath('mysli/util/curl'))
+        && $c->save();
+}
+
+function disable() {
+    $c = config::select('mysli/util/curl');
+    return dir::remove(fs::datpath('mysli/util/curl'))
+        && $c->destroy();
+}
