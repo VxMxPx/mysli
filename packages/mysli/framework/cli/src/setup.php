89ac7177fc494fcf5e25c94b8552715e50df3e4c
Marko <m@gaj.st>
2015-02-18 02:05:43 +0100

    Updated to work with new autoloader.

--- packages/mysli/framework/cli/src/setup.php
+++ packages/mysli/framework/cli/src/setup.php
@@ -3,23 +3,26 @@
 namespace mysli\framework\cli\setup;
 
 __use(__namespace__, '
-    ./util  AS  cutil
     mysli.framework.fs/fs,file
 ');
 
-function enable() {
+function enable()
+{
     $dot = file::read(__DIR__ . '/../data/dot.tpl');
+
     $dot = str_replace(
         '{{PKGPATH}}',
         '/' . fs::relative_path(fs::pkgpath(), fs::datpath()),
-         $dot);
+        $dot
+    );
 
-    if (file::write(fs::datpath('dot'), $dot)) {
-        cutil::execute('cd %s && chmod +x dot', [fs::datpath()]);
+    if (file::write(fs::datpath('dot'), $dot))
+    {
+        exec(vsprintf('cd %s && chmod +x dot', [fs::datpath()]));
         return true;
-    } else {
-        return false;
     }
+    else
+        return false;
 }
 
 function disable() {
