ba750a8d2299c9861d7aaf8bda3d24f6f48bff83
Marko <m@gaj.st>
2015-03-01 14:50:38 +0100

    Fixed: selfrelease, phar now functional.

--- packages/mysli/framework/core/src/setup.php
+++ packages/mysli/framework/core/src/setup.php
@@ -2,40 +2,33 @@
 
 namespace mysli\framework\core\setup;
 
-function enable($pkgpath, $datpath) {
-
+function enable($pkgpath, $datpath)
+{
     $pkgpath = rtrim($pkgpath, '\\/');
     $datpath = rtrim($datpath, '\\/');
     $tmppath = $datpath.'/temp';
-    // Get self path
-    $selfrelease = __FILE__;
-    if (substr($selfrelease, -5) === '.phar') {
-        $selfrelease = substr(basename($selfrelease), 0, -5);
-    } else {
-        $selfrelease = 'mysli/framework/core';
-    }
 
+    // Get self release
+    $selfrelease = __DIR__;
+    if (substr($selfrelease, 0, 7) === 'phar://')
+        $selfrelease = substr( basename(dirname($selfrelease)), 0, -5);
+    else
+        $selfrelease = 'mysli/framework/core';
 
     // Create DATA directory
-    if (!is_dir($datpath)) {
-        if (!mkdir($datpath, 0777, true)) {
+    if (!is_dir($datpath))
+        if (!mkdir($datpath, 0777, true))
             throw new \Exception('Cannot create `data` directory!', 2);
-        }
-    }
 
     // Create boot directory
-    if (!is_dir($datpath . '/boot')) {
-        if (!mkdir($datpath . '/boot')) {
+    if (!is_dir($datpath . '/boot'))
+        if (!mkdir($datpath . '/boot'))
             throw new \Exception('Cannot create `boot` directory.', 3);
-        }
-    }
 
     // Crete TEMP directory
-    if (!is_dir($tmppath)) {
-        if (!mkdir($tmppath)) {
+    if (!is_dir($tmppath))
+        if (!mkdir($tmppath))
             throw new \Exception('Cannot create `temp` directory.', 4);
-        }
-    }
 
     // Writte boot file
     return (bool) file_put_contents(
