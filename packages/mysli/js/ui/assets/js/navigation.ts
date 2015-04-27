799451e74b74792b561ceb2cae09c94849c2b3a3
Marko <m@gaj.st>
2015-04-27 19:03:18 +0200

    Kind of a functional. Navigation will add buttons. Styling off.

--- packages/mysli/js/ui/assets/js/navigation.ts
+++ packages/mysli/js/ui/assets/js/navigation.ts
@@ -1,27 +1,38 @@
+/// <reference path="button.ts" />
+/// <reference path="box.ts" />
 /// <reference path="widget.ts" />
 /// <reference path="_inc.common.ts" />
 module mysli.js.ui {
     export class Navigation extends Widget {
 
-        protected static allowed_styles: string[] = ['default', 'alt'];
-        private static collection: common.Arr = new common.Arr();
+        protected collection: common.Arr = new common.Arr();
+        protected container: Box;
 
-        constructor(options: {items: any}) {
+        constructor(items: any, options: any = {}) {
             super(options);
+            this.container = new Box(options);
+            this.$element = this.container.element;
+            this.element.addClass('ui-navigation');
 
             this.events = common.mix({
-                // When any navigation item is clicked
-                // ( string id, object this )
+                // Respond to a navigation element click!
+                // => ( id: string, event: any, navigation: Navigation )
                 action: {}
             }, this.events);
 
-            this.element.on('click', '.ui-navigation-item', function(e) {
-                var id: string;
-                e.stopPropagation();
-                id = e.currentTarget['id'].substr(6);
-                this.trigger('action', [id]);
-            });
+            for (var item in items) {
+                if (items.hasOwnProperty(item)) {
+                    this.container.push(Navigation.produce(items[item], item, this), item);
+                }
+            }
         }
 
+        private static produce(title: string, id: string, sender: Navigation): Widget {
+            var button: Button = new Button({label: title});
+            button.connect('click', function (e) {
+                this.trigger('action', [id, e]);
+            }.bind(sender));
+            return button;
+        }
     }
 }
