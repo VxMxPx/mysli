ed8ecb7c92d3bbc461a62d3fc40b9e842c17715c
Marko <m@gaj.st>
2015-04-28 19:05:25 +0200

    Support for padding.

--- packages/mysli/js/ui/assets/js/cell.ts
+++ packages/mysli/js/ui/assets/js/cell.ts
@@ -11,13 +11,11 @@ module mysli.js.ui {
             this.parent = parent;
             this.$cell = $cell;
 
-            // Set default values
-            this.prop = {
-                visible: true
-            };
-
-            // Apply new values if needed
-            this.visible = options.visible || true;
+            this.prop = new common.Prop({
+                visible: true,
+                padding: false
+            }, this);
+            this.prop.push(options, ['visible', 'padding']);
         }
 
         /**
@@ -28,6 +26,28 @@ module mysli.js.ui {
          */
         animate(what: any, duration: number = 500, callback: any = false): void {
             this.$cell.animate(what, duration, callback);
+        }
+
+        // Get/set padded
+        get padding(): boolean|any[] {
+            return this.prop.padding;
+        }
+        set padding(value: boolean|any[]) {
+            var positions: string[] = ['top', 'right', 'bottom', 'left'];
+
+            this.$cell.css('padding', '');
+
+            if (typeof value === 'boolean') {
+                value = [value, value, value, value];
+            }
+
+            for (var i=0; i<positions.length; i++) {
+                if (typeof value[i] === 'number') {
+                    this.$cell.css(`padding-${positions[i]}`, value[i]);
+                } else {
+                    this.$cell[value[i] ? 'addClass' : 'removeClass'](`pad${positions[i]}`);
+                }
+            }
         }
 
         // Get/set visibility
