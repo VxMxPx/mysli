cdd5414fbca7baad5de55a721e5da73809e8b1dc
Marko <m@gaj.st>
2015-04-28 19:06:26 +0200

    Set style to flat on insert. Update to new prop style.

--- packages/mysli/js/ui/assets/js/titlebar.ts
+++ packages/mysli/js/ui/assets/js/titlebar.ts
@@ -2,10 +2,15 @@
 
 module mysli.js.ui {
     export class Titlebar extends Box {
-        constructor(options: any={}) {
+        constructor(options: any = {}) {
             options.orientation = Box.VERTICAL;
             super(options);
             this.element.addClass('ui-titlebar');
+        }
+
+        insert(...options): Widget {
+            options[0].flat = true;
+            return super.insert.apply(this, options);
         }
     }
 }
