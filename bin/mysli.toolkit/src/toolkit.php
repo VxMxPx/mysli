e304dfa99a9198ab3f5ab89b31026093f4d95343
Marko <m@gaj.st>
2015-07-05 16:59:37 +0200

    Update: shutdown accepts code. Added: documentation.

--- bin/mysli.toolkit/src/toolkit.php
+++ bin/mysli.toolkit/src/toolkit.php
@@ -8,18 +8,28 @@ namespace mysli\toolkit; class toolkit
     /**
      * Called when a normal shutdown happened.
      * This will load shutdown, and trigger standard shutdown event.
+     * --
+     * @param integer $code
+     *        Code to be used for exit (and when even is triggered).
+     *        If you're unsure leave it to be the default value which is `0`.
+     * --
+     * @event toolkit::shutdown ( integer $code )
      */
-    static function shutdown()
+    static function shutdown($code=0)
     {
         log::info('System is about to shutdown. Bye! :)', __CLASS__);
-        event::trigger("toolkit::shutdown");
+        event::trigger("toolkit::shutdown", [$code]);
 
         echo log_to_html( log::get() );
+
+        exit($code);
     }
 
     /**
      * Emergency shutdown, call this to imediately stop the system.
      * This will write the panic log, trigger panic event and then exit.
+     * --
+     * @event toolkit::panic ()
      */
     static function panic()
     {
