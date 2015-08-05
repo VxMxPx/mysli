edb907d1208c91688dcb2522e8281e031aab15f9
Marko <m@gaj.st>
2015-08-05 22:06:54 +0200

    Added: support for new parser and `static` option.

--- bin/mysli.tplp/script/template.php
+++ bin/mysli.tplp/script/template.php
@@ -26,9 +26,16 @@ namespace mysli\tplp\root\script; class template
         ->create_parameter('PACKAGE', [
             'required' => true,
             'help'     => 'Package\'s templates to be parsed, in format: '.
-                            '`vendor.package`. '.
-                            'Alternatively relative path to the templates root '.
-                            'can be used. Use `./` for current directory.'
+                        '`vendor.package`. '.
+                        'Alternatively relative path to the templates root '.
+                        'can be used. Use `./` for current directory.'
+        ])
+        ->create_parameter('--static/-s', [
+            'type' => 'boolean',
+            'def'  => false,
+            'help' => 'This will produce complete template with all includes '.
+                    'merged into final template file. This will provide a bit of '.
+                    'performance gain, but none of the includes will be replaceable.'
         ])
         ->create_parameter('--watch/-w', [
             'type' => 'boolean',
@@ -39,12 +46,12 @@ namespace mysli\tplp\root\script; class template
         if (null !== ($r = prog::validate_and_print($prog, $args)))
             return $r;
 
-        list($package, $watch) = $prog->get_values('package', '--watch');
+        list($package, $static, $watch) = $prog->get_values('package', '-s', '-w');
 
         // Package to path...
         $path = static::resolve_path($package);
 
-        return static::parse($path, $watch);
+        return static::parse($path, $static, $watch);
     }
 
     /*
@@ -55,11 +62,12 @@ namespace mysli\tplp\root\script; class template
      * Parse templates in particular path, and watch for change.
      * --
      * @param string  $path
+     * @param boolean $static
      * @param boolean $path
      * --
      * @return boolean
      */
-    protected static function parse($path, $watch)
+    protected static function parse($path, $static, $watch)
     {
         // Check if package // Dir exists...
         if (!$path || !fs\dir::exists($path))
@@ -72,8 +80,11 @@ namespace mysli\tplp\root\script; class template
         if (!fs\dir::exists("{$path}/~dist"))
             fs\dir::create("{$path}/~dist");
 
+        $parser = new parser($path);
+
         // Wait for changes
-        return fs\file::observe($path, function ($changes) use ($path, $watch)
+        return fs\file::observe($path, function ($changes)
+            use ($parser, $static, $path, $watch)
         {
             // Watch only for specific changes
             foreach ($changes as $file => $change)
@@ -82,14 +93,14 @@ namespace mysli\tplp\root\script; class template
                 $rfile = substr($file, strlen($path)+1);
 
                 // Dist filename and path
-                $rpfile = substr($rfile, 0, -9).'.php'; // Cut .tpl.html
+                $rpfile = substr($rfile, 0, -9).($static?'.php':'.tpl.php'); // Cut .tpl.html
                 $dspath = "{$path}/~dist/{$rpfile}";
 
                 // Print action and file...
                 ui::info(ucfirst($change['action']), $rfile);
 
                 // Finally process file...
-                if (substr(basename($file), 0, 1) === '_')
+                if (substr(basename($file), 0, 1) === '_' && $static)
                 {
                     // Reload all files, layout was changed... but only if we're
                     // watching, if not, just skip...
@@ -118,7 +129,10 @@ namespace mysli\tplp\root\script; class template
                     try
                     {
                         // Process
-                        $contents = parser::file($rfile, $path);
+                        $contents = $parser->file($rfile);
+
+                        if ($static)
+                            $contents = $parser->extend($contents);
 
                         fs\file::create_recursive($dspath, true);
                         if (fs\file::write($dspath, $contents))
