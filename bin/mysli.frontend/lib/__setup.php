823d0fd8dc004b4cae864d876a56be353492b325
Marko <m@gaj.st>
2015-08-07 19:49:55 +0200

    Added: frontend add theme shortcut.

--- bin/mysli.frontend/lib/__setup.php
+++ bin/mysli.frontend/lib/__setup.php
@@ -5,16 +5,21 @@ namespace mysli\frontend; class __setup
     const __use = 'mysli.toolkit.{
         config,
         fs.fs -> fs,
-        fs.dir -> dir
+        fs.dir -> dir,
+        fs.file -> file
     }';
 
     static function enable()
     {
-        return dir::create(fs::cntpath('themes'));
+        return dir::create(fs::cntpath('themes/default')) &&
+            file::write(
+                fs::cntpath('themes/default/theme.ym'),
+                'source: [ mysli.toolkit, assets/theme ]'
+            );
     }
 
     static function disable()
     {
-        return dir::remove(fs::cntpath('themes'));
+        return dir::remove(fs::cntpath('themes/default'));
     }
 }
