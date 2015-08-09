7e670aa55afd5788cb440ea46d9cddc9c9d3800b
Marko <m@gaj.st>
2015-08-09 13:46:17 +0200

    Added: all script's methods.

---
+++ bin/mysli.frontend/script/theme.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace mysli\frontend\root\script; class theme
+{
+    const __use = '
+        .{ theme -> lib.theme }
+        mysli.toolkit.cli.{
+            prog, ui
+        }
+    ';
+
+
+    /**
+     * Run theme cli.
+     * --
+     * @param array $args
+     */
+    static function __run(array $args)
+    {
+        /*
+        Set parameters.
+         */
+        $prog = new prog(
+            'Mysli Frontend Theme',
+            'Manage Themes Manager.',
+            'mysli.frontend.theme'
+        );
+
+        $prog
+        ->create_parameter('-a/--activate', [
+            'help' => 'Activate particular theme.',
+        ])
+        ->create_parameter('--list', [
+            'type'    => 'boolean',
+            'exclude' => $prog->get_parameters('activate'),
+            'help'    => 'Display a list of themes. Available options.',
+        ])
+        ->create_parameter('--meta', [
+            'exclude' => $prog->get_parameters('activate', 'list'),
+            'help'    => 'Display meta information for particular theme.'
+        ]);
+
+        if (null !== ($r = prog::validate_and_print($prog, $args)))
+            return $r;
+
+        $value = $prog->get_option_at(0, 'value');
+
+        switch ($prog->get_option_at(0, 'name'))
+        {
+            case 'activate':
+                return static::activate($value);
+
+            case 'list':
+                return static::do_list();
+
+            case 'meta':
+                return static::meta($value);
+
+            default:
+                ui::warning('Invalid command, use --help to see available commands.');
+                return false;
+        }
+    }
+
+    /*
+    --- Protected --------------------------------------------------------------
+     */
+
+    /**
+     * Set active theme.
+     * --
+     * @param string $theme
+     * --
+     * @return boolean
+     */
+    protected static function activate($theme)
+    {
+        try
+        {
+            if (!lib\theme::set_active($theme))
+            {
+                ui::error("FAILED");
+                return false;
+            }
+            else
+            {
+                ui::success('OK', "Theme activated.");
+                return true;
+            }
+        }
+        catch (\Exception $e)
+        {
+            ui::error("FAILED", $e->getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Print theme's meta.
+     * --
+     * @param string $theme
+     * --
+     * @return boolean
+     */
+    protected static function meta($theme)
+    {
+        $meta = lib\theme::get_meta($theme);
+        ui::t(
+            "<title>Meta for {$theme}</title>\n\n<al>{meta}</al>",
+            ['meta' => $meta]
+        );
+        return true;
+    }
+
+    /**
+     * Print list of themes.
+     * --
+     * @return boolean
+     */
+    protected static function do_list()
+    {
+        $active = lib\theme::get_active();
+        $all    = lib\theme::get_list();
+        $list   = [];
+
+        foreach ($all as $id => $meta)
+        {
+            if ($active === $id)
+                $list[$id] = ui::strong('*'.$meta['theme'], true);
+            else
+                $list[$id] = $meta['theme'];
+        }
+
+        ui::t(
+            "<title>List of themes.</title>\n\n".
+            "<al>{list}</al>",
+            [
+                'list' => $list
+            ]
+        );
+        return true;
+    }
+
+}
