d55b6a37e51e0de33d97c8ac3a72ae5bcc9460b8
Marko <m@gaj.st>
2015-08-09 14:48:34 +0200

    Update: it will render proper 404 template.

--- bin/mysli.frontend/lib/frontend.php
+++ bin/mysli.frontend/lib/frontend.php
@@ -3,8 +3,16 @@
 namespace mysli\frontend; class frontend
 {
     const __use = '
-        mysli.tplp, mysli.toolkit.{ response, output }
+        .{ theme }
+        mysli.tplp
+        mysli.i18n
+        mysli.toolkit.{ response, output, config }
     ';
+
+    static function __init()
+    {
+        i18n::select('mysli.frontend');
+    }
 
     /**
      * Render particular template with content, and set output.
@@ -25,7 +33,48 @@ namespace mysli\frontend; class frontend
      */
     static function error404($route)
     {
+        static::set_language($route);
+
+        $theme = theme::get_meta(theme::get_active());
+
+        if (!is_array($theme))
+        {
+            return false;
+        }
+
+        $template = tplp::select($theme['absolute_path']);
+        $language = i18n::select('mysli.frontend');
+        $language->load($theme['absolute_path'].'/i18n');
+        $template->set_translator($language);
+
         response::set_status(404);
-        die('404');
+
+        if ($template->has('error404'))
+        {
+            output::set($template->render('error404'));
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    /*
+    --- Protected --------------------------------------------------------------
+     */
+
+    protected static function set_language($route)
+    {
+        if ($route->option('i18n.language'))
+            $langkey = $route->option('i18n.language');
+        else
+            $langkey = config::select('mysli.frontend', 'locale.default');
+
+        $locales = config::select('mysli.frontend', 'locale.accept');
+        $language = isset($locales[$langkey]) ? $locales[$langkey] : $langkey;
+
+        i18n::select('mysli.frontend')->primary($language);
     }
 }
