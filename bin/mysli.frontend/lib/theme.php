9576551ec9173f097d4ea1ba3d5a0573a5d74774
Marko <m@gaj.st>
2015-08-09 13:46:00 +0200

    Added: meta, set_active, has, ...

--- bin/mysli.frontend/lib/theme.php
+++ bin/mysli.frontend/lib/theme.php
@@ -8,12 +8,11 @@
 namespace mysli\frontend; class theme
 {
     const __use = '
-        .{
-            exception.theme
-        }
+        .{ exception.theme }
         mysli.toolkit.{
             ym
             pkg
+            config
             fs.fs -> fs
             fs.dir -> dir
             fs.file -> file
@@ -23,9 +22,6 @@ namespace mysli\frontend; class theme
     /**
      * List available themes.
      * --
-     * @throws mysli\frontend\exception\theme
-     *         10 File specefiled in source not found.
-     * --
      * @return array
      */
     static function get_list()
@@ -34,48 +30,14 @@ namespace mysli\frontend; class theme
 
         foreach (fs::ls(fs::cntpath('themes')) as $directory)
         {
-            $themeym = fs::cntpath('themes', $directory, 'theme.ym');
-            if (!file::exists($themeym))
+            $meta = static::get_meta($directory);
+
+            if (!$meta)
             {
                 continue;
             }
 
-            $mtheme = ym::decode_file($themeym);
-
-            /*
-            Source theme from different directory
-             */
-            if (isset($mtheme['source']))
-            {
-                $source = $mtheme['source'];
-
-                // Source from package's root
-                if (is_array($source))
-                {
-                    $source = static::get_pkg_source($source[0], $source[1]);
-                }
-
-                $sthemeym = fs::ds($source, 'theme.ym');
-
-                if (!file::exists($sthemeym))
-                    throw new exception\theme(
-                        "File specefiled in source not found: `{$source}`.", 10
-                    );
-
-                $mtheme = array_merge_recursive(
-                    $mtheme,
-                    ym::decode_file($sthemeym)
-                );
-
-                $mtheme['absolute_path'] = $source;
-            }
-            else
-            {
-                $mtheme['absolute_path'] = fs::cntpath('themes', $directory);
-            }
-
-            $mtheme['id'] = $directory;
-            $list[$directory] = $mtheme;
+            $list[$meta['id']] = $meta;
         }
 
         return $list;
@@ -86,11 +48,35 @@ namespace mysli\frontend; class theme
      * --
      * @param string $theme Theme's ID.
      * --
+     * @throws mysli\frontend\exception\theme 10 Cannot get meta.
+     * --
      * @return boolean
      */
     static function set_active($theme)
     {
+        if ($theme === static::get_active())
+        {
+            return true;
+        }
 
+        $meta = static::get_meta($theme);
+
+        if (!$meta)
+        {
+            throw new exception\theme("Cannot get meta for: `{$theme}`.", 10);
+        }
+
+        // Something to publish?
+        $publish = fs::ds($meta['absolute_path'], 'public');
+        if (dir::exists($publish))
+        {
+            dir::copy($publish, fs::pubpath('themes', $theme));
+        }
+
+        $config = config::select('mysli.frontend');
+        $config->set('theme.active', $theme);
+
+        return $config->save();
     }
 
     /**
@@ -100,7 +86,79 @@ namespace mysli\frontend; class theme
      */
     static function get_active()
     {
+        return config::select('mysli.frontend', 'theme.active');
+    }
 
+    /**
+     * Check if particular theme exists.
+     * --
+     * @param string $theme
+     * --
+     * @return boolean
+     */
+    static function has($theme)
+    {
+        return file::exists(
+            fs::cntpath('themes', $theme, 'theme.ym')
+        );
+    }
+
+    /**
+     * Get (merged) meta file for particular theme.
+     * --
+     * @param string $theme
+     * --
+     * @throws mysli\frontend\exception\theme
+     *         10 File specefiled in source not found.
+     * --
+     * @return array
+     */
+    static function get_meta($theme)
+    {
+        $themeym = fs::cntpath('themes', $theme, 'theme.ym');
+
+        if (!file::exists($themeym))
+        {
+            return null;
+        }
+
+        $mtheme = ym::decode_file($themeym);
+
+        /*
+        Source theme from different directory
+         */
+        if (isset($mtheme['source']))
+        {
+            $source = $mtheme['source'];
+
+            // Source from package's root
+            if (is_array($source))
+            {
+                $source = static::get_pkg_source($source[0], $source[1]);
+            }
+
+            $sthemeym = fs::ds($source, 'theme.ym');
+
+            if (!file::exists($sthemeym))
+                throw new exception\theme(
+                    "File specefiled in source not found: `{$source}`.", 10
+                );
+
+            $mtheme = array_merge_recursive(
+                $mtheme,
+                ym::decode_file($sthemeym)
+            );
+
+            $mtheme['absolute_path'] = $source;
+        }
+        else
+        {
+            $mtheme['absolute_path'] = fs::cntpath('themes', $theme);
+        }
+
+        $mtheme['id'] = $theme;
+
+        return $mtheme;
     }
 
     /*
