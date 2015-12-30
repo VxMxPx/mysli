f3eeba2824e9c565cd4dab7dda6de2b1f86b3a38
Marko <m@gaj.st>
2015-12-30 13:29:58 +0100

    Update: complex blockquote test.

--- bin/mysli.markdown/tests/parser/process.blockquote-containing.t.php
+++ bin/mysli.markdown/tests/parser/process.blockquote-containing.t.php
@@ -19,4 +19,20 @@ $markdown = <<<MARKDOWN
 >     return shell_exec("echo \$input | \$markdown_script");
 MARKDOWN;
 
-return assert::equals(markdown::process($markdown), '');
+return assert::equals(markdown::process($markdown),
+'<blockquote>
+    <h2>This is a header.</h2>
+    <ol>
+        <li>
+            <p>This is the first list item.</p>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+        </li>
+        <li>This is the second list item.</li>
+    </ol>
+    <p>Here\'s some example code:</p>
+    <code>return shell_exec("echo $input | $markdown_script");</code>
+ </blockquote>');
