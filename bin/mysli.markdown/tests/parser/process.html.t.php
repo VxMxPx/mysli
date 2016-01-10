cf5157c14cca8fdf66d137ec9621444afcdfe5e1
Marko <m@gaj.st>
2016-01-10 03:52:09 +0100

    Add: HTML (tags) test.

---
+++ bin/mysli.markdown/tests/parser/process.html.t.php
@@ -0,0 +1,47 @@
+<?php
+
+#: Before
+use mysli\markdown;
+
+#: Test Simple HTML tags
+$markdown = <<<MARKDOWN
+<small>
+    Also, _inline_ tags will be matched **here**.
+</small>
+
+<div>This is a div.</div>
+
+<div>
+There are some _inline_ tags here too, which will be skipped.
+</div>
+
+<a href="#top">Top</a>
+MARKDOWN;
+
+return assert::equals(markdown::process($markdown),
+'<p><small>
+    Also, <em>inline</em> tags will be matched <strong>here</strong>.
+    </small></p>
+<div>This is a div.</div>
+<div>
+There are some _inline_ tags here too, which will be skipped.
+</div>
+<p><a href="#top">Top</a></p>');
+
+#: Test Process Within HTML Tags
+$markdown = <<<MARKDOWN
+<small>
+    - one
+    - two
+    - three
+    - four
+</small>
+MARKDOWN;
+
+return assert::equals(markdown::process($markdown),
+'<p><small>
+    - one
+    - two
+    - three
+    - four
+    </small></p>');
