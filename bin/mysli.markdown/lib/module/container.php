5f5181711942730c679822e489219bfc2d417fcb
Marko <m@gaj.st>
2016-01-16 14:47:01 +0100

    Add: container module.

---
+++ bin/mysli.markdown/lib/module/container.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace mysli\markdown\module; class container extends std_module
+{
+    function process($at)
+    {
+        $opened = null;
+        $lines = $this->lines;
+
+        while ($lines->has($at))
+        {
+            if ($lines->get_attr($at, 'no-process'))
+            {
+                $at++;
+                continue;
+            }
+
+            $line = $lines->get($at);
+
+            if ($opened && preg_match('/^:::$/', $line))
+            {
+                // Open tag...
+                list($pat, $classes) = $opened;
+                $lines->erase($pat, true);
+                $lines->set_tag($pat, ['div', false]);
+                $lines->set_attr($pat, [
+                    'no-process' => true,
+                    'html-attributes' => [
+                        'div' => [ 'class="'.str_replace('.', ' ', $classes).'"' ]
+                    ]
+                ]);
+
+                // Close tag...
+                $lines->erase($at, true);
+                $lines->set_tag($at, [false, 'div']);
+                $lines->set_attr($at, 'no-process', true);
+
+                $opened = null;
+            }
+            else
+            {
+                if (preg_match('/^:::\s*([a-z0-9\.]+)$/i', $line, $match))
+                {
+                    $opened = [$at, $match[1]];
+                }
+            }
+            $at++;
+        }
+    }
+}
