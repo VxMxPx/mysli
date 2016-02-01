d32de3bea562f342db17ad7e31c819baf7aedae4
Marko <m@gaj.st>
2016-02-01 13:57:52 +0100

    Add: links can set local sources.

--- bin/mysli.markdown/lib/module/link.php
+++ bin/mysli.markdown/lib/module/link.php
@@ -2,6 +2,9 @@
 
 namespace mysli\markdown\module; class link extends std_module
 {
+    // Rewrite local url(s) (those which does not start with [a-z]://)
+    protected $local_url = '';
+
     function process($at)
     {
         $lines = $this->lines;
@@ -13,6 +16,11 @@ namespace mysli\markdown\module; class link extends std_module
 
                 list($_, $_, $txt, $url) = $match;
                 $url = str_replace('"', '%22', $url);
+
+                if (!preg_match('/^[a-z]{2,5}:\/\/.*?$/', $url))
+                {
+                    $url = $this->get_local_url($url);
+                }
 
                 if ($match[1] === '!')
                 {
@@ -30,9 +38,17 @@ namespace mysli\markdown\module; class link extends std_module
             },
         ];
 
-        $this->process_inline($regbag, $at, [
-            // 'html-tag-opened' => true,
-            // 'html-tag-closed' => true
-        ]);
+        $this->process_inline($regbag, $at);
+    }
+
+    function set_local_url($url)
+    {
+        $this->local_url = rtrim($url, '/');
+    }
+
+    function get_local_url($uri=null)
+    {
+        if (!$this->local_url) return $uri;
+        return $this->local_url.'/'.ltrim($uri, '/');
     }
 }
