2035d6066e7b017e1100f3a54c6ef59114c2c071
Marko <m@gaj.st>
2016-02-15 12:31:01 +0100

    Add: page __tplp with url method.

---
+++ bin/mysli.page/lib/__tplp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace mysli\page; class __tplp
+{
+    const __use = <<<fin
+    mysli.frontend.{ __tplp -> frontend.tplp }
+    mysli.toolkit.{ route }
+fin;
+
+    /**
+     * Return internal blog URL.
+     * --
+     * @param string $uri
+     * @param string $type URI type: post|ppost|tag|archive
+     * --
+     * @return string
+     */
+    static function url($uri='')
+    {
+        $url = route::to_url(
+            "mysli.page.frontend::page",
+            [ $uri ],
+            false
+        );
+
+        return frontend\tplp::url($url);
+    }
+}
