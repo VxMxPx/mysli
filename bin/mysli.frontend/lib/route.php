0141adc1056c596a9cea94fee195432f12552f84
Marko <m@gaj.st>
2016-02-17 19:59:42 +0100

    Cleanup. Add: title, ...

--- bin/mysli.frontend/lib/route.php
+++ bin/mysli.frontend/lib/route.php
@@ -7,6 +7,13 @@ namespace mysli\frontend; class route
         mysli.toolkit.{ output, response }
     ';
 
+    /**
+     * Throws 404!
+     * --
+     * @param integer $n
+     * --
+     * @return boolean
+     */
     static function error($n)
     {
         if ($n !== 404)
@@ -15,10 +22,15 @@ namespace mysli\frontend; class route
         }
 
         response::set_status(response::status_404_not_found);
-        frontend::render([
-            'error_404',
-            ['mysli.frontend', 'error_404']
-        ]);
+        frontend::render(
+            ['error_404', ['mysli.frontend', 'error_404']],
+            [
+                'front' => [
+                    'subtitle' => '404',
+                    'type'     => 'error-404'
+                ]
+            ]
+        );
 
         return true;
     }
