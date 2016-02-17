b819e9102c36f408a897298d624de4fcf35df11b
Marko <m@gaj.st>
2016-02-17 20:00:46 +0100

    Add: default variables.

--- bin/mysli.blog/lib/frontend.php
+++ bin/mysli.blog/lib/frontend.php
@@ -9,6 +9,11 @@ namespace mysli\blog; class frontend
     mysli.frontend.{ frontend -> fe }
 fin;
 
+    /**
+     * Render blog archive.
+     * --
+     * @return boolean
+     */
     static function archive()
     {
         config::select('mysli.blog', 'cache.reload-on-access') and blog::refresh_list();
@@ -18,7 +23,14 @@ fin;
 
         fe::render(['blog-archive', ['mysli.blog', 'archive']], [
             'front' => [
-                'title' => i18n::select(['mysli.blog', 'en', null], 'ARCHIVE')
+                'subtitle' => i18n::select(['mysli.blog', 'en', null], 'ARCHIVE'),
+                'type'     => 'blog-archive'
+            ],
+            'blog' => [
+                'categories' => config::select('mysli.blog', 'tags.to-categories'),
+                'is_archive' => true,
+                'is_tag'     => false,
+                'tag'        => null,
             ],
             'posts' => $list
         ]);
@@ -26,6 +38,13 @@ fin;
         return true;
     }
 
+    /**
+     * Render archive for a particular tag.
+     * --
+     * @param string $id
+     * --
+     * @return boolean
+     */
     static function tag($id)
     {
         config::select('mysli.blog', 'cache.reload-on-access') and blog::refresh_list();
@@ -33,19 +52,49 @@ fin;
         $list = blog::list_by_tag($id);
         krsort($list);
 
+        $categories = config::select('mysli.blog', 'tags.to-categories');
+
         fe::render(['blog-archive', ['mysli.blog', 'archive']], [
-            'front' => [ 'title' => ucfirst($id) ],
+            'front' => [
+                'subtitle' => ucfirst($id),
+                'type'     => 'blog-tag',
+                'quid'     => 'blog-tag-'.$id,
+            ],
+            'blog' => [
+                'categories' => $categories,
+                'is_archive' => false,
+                'is_tag'     => true,
+                'tag'        => $id
+            ],
             'posts' => $list
         ]);
 
         return true;
     }
 
+    /**
+     * Render a particular page of a post.
+     * --
+     * @param integer $year
+     * @param string  $id
+     * @param string  $page
+     * --
+     * @return boolean
+     */
     static function ppost($year, $id, $page)
     {
         return static::post($year, $id, $page);
     }
 
+    /**
+     * Render a particular post.
+     * --
+     * @param integer $year
+     * @param string  $id
+     * @param string  $page
+     * --
+     * @return boolean
+     */
     static function post($year, $id, $page='_default')
     {
         if (!blog::has($year, $id))
@@ -62,6 +111,7 @@ fin;
 
         $c = config::select('mysli.blog');
 
+        // Do cache
         if ($c->get('cache.reload-on-access') && !$post->is_cache_fresh())
         {
             $post->make_cache();
@@ -78,6 +128,7 @@ fin;
             }
         }
 
+        // Check publish status
         if (!$post->get('published', true))
         {
             // Final chance to view in dev-access
@@ -89,8 +140,19 @@ fin;
         }
 
         fe::render(['blog-post', ['mysli.blog', 'post']], [
-            'front' => [ 'title' => $post->get('title') ],
-            'post'  => $post->as_array($page)
+            'front' => [
+                'subtitle' => $post->get('title'),
+                'type'     => 'blog-post',
+                'quid'     => 'post-'.str_replace(['/', '.'], '-', $post->get_quid())
+            ],
+            'blog' => [
+                'categories' => config::select('mysli.blog', 'tags.to-categories'),
+                'is_archive' => false,
+                'is_tag'     => false,
+                'tag'        => null,
+            ],
+            'page' => $post->as_array($page),
+            'post' => $post->as_array(null),
         ]);
 
         return true;
