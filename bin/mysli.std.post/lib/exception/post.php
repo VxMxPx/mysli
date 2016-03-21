6caef5b786a4657bd19f3608af6ed2718aa36057
Marko <m@gaj.st>
2016-03-21 16:10:00 +0100

    Remove: std post (as replaced by content).

--- bin/mysli.std.post/lib/exception/post.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-namespace mysli\std\post\exception; class post extends \mysli\toolkit\exception\base {}
-
--- bin/mysli.std.post/tests/post/get_quid.t.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-#: Define Post
-$post = new post('blog/2016/full-post-slug');
-
-#: Test Get QUID
-#: Use Post
-#: Expect String blog/2016/full-post-slug
-return $post->get_quid();
