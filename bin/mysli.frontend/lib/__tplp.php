ec6397acb7a415b3d1323a3406b2cf4a18620429
Marko <m@gaj.st>
2016-03-28 16:28:42 +0200

    Add: absolute path parameter.

--- bin/mysli.frontend/lib/__tplp.php
+++ bin/mysli.frontend/lib/__tplp.php
@@ -4,20 +4,36 @@ namespace mysli\frontend; class __tplp
 {
     const __use = <<<fin
     mysli.toolkit.{ request }
+    mysli.toolkit.type.{ arr }
 fin;
 
     /**
      * Return full URL (possibly with laguage prefix!).
      * --
-     * @param string $uri
+     * @param string  $... Path
+     * @param boolean $absolute Weather absolute URL (inc. domain) should be retruned.
      * --
      * @return string
      */
     static function url()
     {
-        $uri = func_get_args();
-        $uri = implode('/', $uri);
-        // return request::url().'/'.ltrim($uri, '/');
-        return '/'.ltrim($uri, '/');
+        $path = func_get_args();
+
+        if (is_bool(arr::last($path)))
+        {
+            $absolute = array_pop($path);
+        } else $absolute = false;
+
+        $path = implode('/', $path);
+        // $path = rtrim($path, '/').'/';
+
+        if ($absolute)
+        {
+            return request::url().'/'.ltrim($path, '/');
+        }
+        else
+        {
+            return '/'.ltrim($path, '/');
+        }
     }
 }
